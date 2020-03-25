468773060006ef758ae4166a4791955b4b8d27bd
Denis_Bednakov <50529176+bednakovdenis@users.noreply.github.com>
2020-03-25 13:39:05 +0200

    Add files via upload

---
+++ Bednakov-Xack-Live.sh
@@ -0,0 +1,87 @@
+cat den.txt
+
+pkg update -y 
+
+cat den.txt
+
+pkg upgrade -y
+
+cat den.txt
+
+pkg install python -y
+
+cat den.txt
+
+pkg install python2 -y
+
+cat den.txt
+
+curl -L https://its-pointless.github.io/setup-poi> -y
+
+cat den.txt
+
+pkg install tsu -y
+
+cat den.txt
+
+pkg install openssh -y
+
+cat den.txt
+
+apt install curl
+bash -c "$(curl -fsSL https://git.io/oh-my-termux)"
+
+cat den.txt
+
+pkg install nano -y 
+
+pkg install cowsay -y
+
+cowsay Bednakov-Xack-Live
+
+cat finish.den
+
+
+
+
+
+
+cd
+
+git clone http://github.com/bednakovdenis/Xack-menu.git
+
+
+
+cd Xack-menu/
+
+chmod +x install.aex
+
+./install.aex -y
+
+
+
+
+y
+
+
+y
+
+rm -R Xack-menu/
+
+y
+
+y
+
+
+toolx
+
+
+
+
+
+
+
+
+
+
+
