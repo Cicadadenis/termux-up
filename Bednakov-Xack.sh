44701ee199e482b3ae018314d74a7077e72957b0
Denis_Bednakov <50529176+bednakovdenis@users.noreply.github.com>
2020-03-25 12:24:59 +0200

    Add files via upload

---
+++ Bednakov-Xack.sh
@@ -0,0 +1,49 @@
+cat den.txt 
+
+curl -L https://its-pointless.github.io/setup-poi>
+
+
+apt update
+apt install tsu
+tsu
+
+apt install openssh -y
+
+apt install curl
+bash -c "$(curl -fsSL https://git.io/oh-my-termux>
+
+cat den.txt
+
+pkg update -y 
+
+cat den.txt
+
+pkg upgrade -y
+
+cat den.txt
+
+pkg install python -y
+
+cat den.txt
+
+pkg install python2 -y
+
+cat den.txt
+
+apt install tsu -y
+
+cat den.txt
+
+apt install openssh -y
+
+apt install curl
+
+cat den.txt
+
+apt install curl
+bash -c "$(curl -fsSL https://git.io/oh-my-termux)
+
+
+exit
+
+exit
